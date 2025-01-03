e885715f5ae2df57d984af1b09187a74a33cbeb3
Patrick Ayres <payres@panoramaed.com>
2025-01-02 22:54:31 -0500

    add git history keymap

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -14,9 +14,13 @@ return {
 
       local icons = require 'mini.icons'
       icons.setup()
+
       local git = require 'mini.git'
       git.setup()
       vim.keymap.set("n", "<leader>gbl", ":lefta vert Git blame -- %<cr>")
+      local rhs = '<Cmd>lua MiniGit.show_at_cursor()<CR>'
+      vim.keymap.set({ 'n', 'x' }, '<Leader>gs', rhs, { desc = 'Show at cursor' })
+
       local diff = require 'mini.diff'
       diff.setup()
       local statusline = require 'mini.statusline'
