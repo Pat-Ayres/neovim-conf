7e96061006627a6410fe6136ed1b0efaf4ddd526
Patrick Ayres <payres@panoramaed.com>
2025-11-19 14:39:31 -0500

    refactor deprecated lsp functionality

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      'saghen/blink.cmp',
+      "saghen/blink.cmp",
       {
         "folke/lazydev.nvim",
         opts = {
@@ -13,9 +13,9 @@ return {
       },
     },
     config = function()
-      local lspconfig = require("lspconfig")
+      -- NOTE: we no longer require("lspconfig"), just the util helper is fine.
       local util = require("lspconfig.util")
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
       -- set a consistent offsetEncoding to ensure all clients match when multiple clients are loaded
       capabilities.offsetEncoding = { "utf-16" }
@@ -26,26 +26,35 @@ return {
         return init_result
       end
 
-      -- language configurations
-      lspconfig.lua_ls.setup { capabilites = capabilities }
+      ---------------------------------------------------------------------------
+      -- Language configurations (new vim.lsp.config/vim.lsp.enable style)
+      ---------------------------------------------------------------------------
 
-      lspconfig.gopls.setup { capabilities = capabilities }
-
-      lspconfig.rust_analyzer.setup { capabilities = capabilities }
-
-      lspconfig.terraformls.setup({
+      vim.lsp.config("lua_ls", {
         capabilities = capabilities,
-        cmd = {
-          "terraform-ls",
-          "serve"
-        },
+      })
+
+      vim.lsp.config("gopls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("rust_analyzer", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("terraformls", {
+        capabilities = capabilities,
+        cmd = { "terraform-ls", "serve" },
         filetypes = {
           "terraform",
           "tf",
           "hcl",
-        }
+        },
       })
-      lspconfig.tflint.setup { capabilities = capabilities }
+
+      vim.lsp.config("tflint", {
+        capabilities = capabilities,
+      })
 
       -- location of a custom gemfile to run ruby-lsp with shared org overcommit gemfile
       local custom_gemfile = vim.fn.expand("~/src/scratch/overcommit-ruby-lsp/Gemfile")
@@ -53,7 +62,7 @@ return {
         vim.notify("❗ Ruby-LSP Gemfile not found: " .. custom_gemfile, vim.log.levels.WARN)
       end
 
-      lspconfig.ruby_lsp.setup {
+      vim.lsp.config("ruby_lsp", {
         capabilities = capabilities,
         filetypes = { "ruby", "eruby" },
         on_init = force_utf16,
@@ -67,7 +76,7 @@ return {
             new_config.cmd_env = { BUNDLE_GEMFILE = custom_gemfile }
           elseif vim.fn.filereadable(root .. "/Gemfile") == 1 then
             -- project has its own Gemfile, use that
-            new_config.cmd = { "bundle", "exec", "ruby-lsp", "stdio" }
+            new_config.cmd     = { "bundle", "exec", "ruby-lsp", "stdio" }
             new_config.cmd_env = { BUNDLE_GEMFILE = root .. "/Gemfile" }
           else
             -- otherwise just invoke the globally-installed LSP
@@ -75,35 +84,50 @@ return {
             new_config.cmd_env = nil
           end
         end,
-      }
+      })
 
       -- use solargraph for goto definition, which is not well supported in ruby-lsp yet
-      lspconfig.solargraph.setup {
+      vim.lsp.config("solargraph", {
         capabilities = capabilities,
-        filetypes    = { "ruby", "eruby" },
-        on_init      = force_utf16,
-        root_dir     = util.root_pattern("Gemfile", ".git"),
-        settings     = {
+        filetypes = { "ruby", "eruby" },
+        on_init = force_utf16,
+        root_dir = util.root_pattern("Gemfile", ".git"),
+        settings = {
           solargraph = {
             diagnostics = false, -- let ruby-lsp handle diagnostics
           },
         },
-      }
+      })
 
+      -- Actually enable all these configs so they attach on their filetypes
+      for _, name in ipairs({
+        "lua_ls",
+        "gopls",
+        "rust_analyzer",
+        "terraformls",
+        "tflint",
+        "ruby_lsp",
+        "solargraph",
+      }) do
+        vim.lsp.enable(name)
+      end
+
+      ---------------------------------------------------------------------------
       -- keybinds
-      vim.keymap.set("n", "[g", ":lua vim.diagnostic.goto_prev()<cr>")
-      vim.keymap.set("n", "]g", ":lua vim.diagnostic.goto_next()<cr>")
+      ---------------------------------------------------------------------------
+      vim.keymap.set("n", "[g", function() vim.diagnostic.jump({ count = -1, float = true }) end)
+      vim.keymap.set("n", "]g", function() vim.diagnostic.jump({ count = 1, float = true }) end)
       vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.show()<cr>")
       vim.keymap.set("n", "<leader>r", ":lua vim.lsp.buf.rename()<cr>")
       vim.keymap.set("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<cr>")
 
-      vim.api.nvim_create_autocmd('LspAttach', {
+      vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local c = vim.lsp.get_client_by_id(args.data.client_id)
           if not c then return end
 
           -- Format the current buffer on save
-          vim.api.nvim_create_autocmd('BufWritePre', {
+          vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = args.buf,
             callback = function()
               vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
@@ -112,5 +136,5 @@ return {
         end,
       })
     end,
-  }
+  },
 }
